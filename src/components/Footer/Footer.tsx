8ca2d4ebbb4196939cc6d525e639b617dbe62e46
Sama Jabri <sama.jabri@outlook.com>
2024-05-14 18:15:42 +0300

    Implemented footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,24 @@
-import PropTypes from "prop-types";
-
 import "./footer.scss";
 
-const Footer = (props) => {
-  return <div>Footer</div>;
-};
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer_upper">
+        <div className="footer_upper-description">
+          <h1>Moviely</h1>
+          <p>
+            Your all time favorite destination for making your Friday movie
+            nights better.
+          </p>
+        </div>
+        <div className="footer_upper-links"></div>
+      </div>
 
-Footer.propTypes = {};
+      <div className="footer_lower">
+        <p>Copyright © 2024 Moviely. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
